8b31965116f095807a11b12b6baedcd688b771eb
Huajun Gu <ghjconan@hotmail.com>
2017-10-29 16:49:01 +0800

    first commit

---
+++ iviewui-snippets/README.md
@@ -0,0 +1,9 @@
+# iviewui-snippets README
+
+A Visual Studio Code extension for easily using iviewui components.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release.
---
+++ iviewui-snippets/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <Row type="flex" justify="[start|end|center|space-between|space-around]" class="code-row-bg">
+    <Col span="4">col-4</Col>
+    <Col span="4">col-4</Col>
+    <Col span="4">col-4</Col>
+    <Col span="4">col-4</Col>
+  </Row>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
